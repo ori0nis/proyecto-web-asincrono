5fa8b1690cc5e3db1ac63f9b71172c2606846f76
ori0nis <oihanegdcl@gmail.com>
2024-10-01 16:59:34 +0200

    finished

--- main.js
+++ main.js
@@ -8,16 +8,5 @@ header.innerHTML = navbar();
 fetchPhotos()
 renderPhotos()
 
-/* const attachEventListeners = () => {
-    const homeButton = document.querySelector(".home");
-    if (homeButton) {
-        // Use a function reference for the click event
-        homeButton.addEventListener("click", () => fetchPhotos("cats"));
-    }
-};
-
-// Call the function to attach event listeners
-attachEventListeners(); */
-
 const homeButton = document.querySelector(".home")
 homeButton.addEventListener("click", () => fetchPhotos("cats"));  
