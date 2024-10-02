56c3be0ec30a3c59bb8a133734c6d1751a0b4547
ori0nis <oihanegdcl@gmail.com>
2024-10-02 14:39:31 +0200

    with better gallery

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -4,6 +4,9 @@ import './gallery.css'
 export const galleryGrid = (photo) => `
  <div class="photo">
     <img src="${photo.urls.small}" alt="${photo.alt_description || 'Photo'}">
+    <div class="like-icon">
+      <img src="./icons/like-icon.png">
+    </div>  
   </div>
 `
 
